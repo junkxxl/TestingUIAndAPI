392ce4f7f0283d00559dfc808b9e6c70cf3f4f8e
Alex <junkxl@mail.ru>
2022-10-28 18:33:05 +0300

    Рефакторинг

--- ModuleAPI/src/main/java/me/junkxxl/testing/testapi/AppConfig.java
+++ ModuleAPI/src/main/java/me/junkxxl/testing/testapi/AppConfig.java
@@ -7,9 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:/application.properties")
 public class AppConfig {
-    @Value("${spring.datasource.URL}")
+    @Value("${URL}")
     public String URL;
     public ObjectMapper objectMapper;
 
