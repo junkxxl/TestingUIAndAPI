04c636cff53130deaa0a98e97d78553d9d4cac71
Alex <junkxl@mail.ru>
2022-11-01 18:09:39 +0300

    Убрал AppConfig

--- ModuleAPI/src/main/java/me/junkxxl/testing/testapi/RestSteps.java
+++ ModuleAPI/src/main/java/me/junkxxl/testing/testapi/RestSteps.java
@@ -1,28 +1,38 @@
 package me.junkxxl.testing.testapi;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
 import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
 import me.junkxxl.testing.model.Car;
 import me.junkxxl.testing.model.House;
 import me.junkxxl.testing.model.User;
 import me.junkxxl.testing.model.house.ParkingPlaces;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import static io.restassured.RestAssured.given;
 
 @Service
 public class RestSteps {
     @Autowired
-    AppConfig appConfig;
+    ObjectMapper objectMapper;
+    @Value("${URL}")
+    private String url;
 
-    @Step("Добавляем пользователя {name} {lastname} ")
+    private RequestSpecification getSpecRequest() {
+        return RestAssured.given().baseUri(url).contentType("application/json").filter(new AllureRestAssured());
+    }
+
+
+    @Step("Добавляем пользователя {name} {lastname}")
     @Attachment
     public User addUser(String name, String lastname, int age, String sex, int money) throws JsonProcessingException {
-        return given().baseUri(appConfig.URL).contentType("application/json").filter(new AllureRestAssured())
-                .body(appConfig.objectMapper.writeValueAsString(
+        return getSpecRequest()
+                .body(objectMapper.writeValueAsString(
                         new User()
                                 .setFirstName(name)
                                 .setSecondName(lastname)
@@ -33,10 +43,11 @@ public class RestSteps {
                 .extract().response().as(User.class);
     }
 
+
     @Step("Запрашиваем данные пользователя по id = {id}")
     @Attachment
     public User getUserId(int id) {
-        return given().baseUri(appConfig.URL).contentType("application/json").filter(new AllureRestAssured())
+        return getSpecRequest()
                 .get("/user/{id}", id).then().log().all().statusCode(200)
                 .extract().response().as(User.class);
     }
@@ -44,7 +55,7 @@ public class RestSteps {
     @Step("Получаем список всех автомобилей")
     @Attachment
     public Car[] getCar() {
-        return given().baseUri(appConfig.URL).contentType("application/json").filter(new AllureRestAssured())
+        return getSpecRequest()
                 .get("/cars").then().log().all().statusCode(200)
                 .extract().response().as(Car[].class);
     }
@@ -52,8 +63,8 @@ public class RestSteps {
     @Step("Добавляем автомобиль марки {mark}, модель {model}")
     @Attachment
     public Car addCar(String engineType, String mark, String model, int price) throws JsonProcessingException {
-        return given().baseUri(appConfig.URL).contentType("application/json").filter(new AllureRestAssured())
-                .body(appConfig.objectMapper.writeValueAsString(
+        return getSpecRequest()
+                .body(objectMapper.writeValueAsString(
                         new Car()
                                 .setEngineType(engineType)
                                 .setMark(mark)
@@ -66,8 +77,8 @@ public class RestSteps {
     @Step("Добавляем дом с ценой = {price}")
     @Attachment
     public House addHouse(int price) throws JsonProcessingException {
-        return given().baseUri(appConfig.URL).contentType("application/json").filter(new AllureRestAssured())
-                .body(appConfig.objectMapper.writeValueAsString(
+        return getSpecRequest()
+                .body(objectMapper.writeValueAsString(
                         new House()
                                 .setFloorCount(3)
                                 .setPrice(price)
@@ -92,20 +103,18 @@ public class RestSteps {
     @Step("Добавляем пользователю с id = {id}, сумму в размере = {amount}")
     @Attachment
     public User addAmountToUserByID(int id, int amount) {
-        return given().baseUri(appConfig.URL).contentType("application/json").filter(new AllureRestAssured())
+        return getSpecRequest()
                 .post("/user/{id}/money/{amount}", id, amount).then().log().all().statusCode(200)
                 .extract().response().as(User.class);
     }
 
     @Step(" Покупаем дом с id = {houseId} , пользователю с id = {userId}")
     public void settleHouseByUserID(int houseId, int userId) {
-        given().baseUri(appConfig.URL).contentType("application/json").filter(new AllureRestAssured())
-                .post("/house/{houseId}/settle/{userId}", houseId, userId).then().log().all().statusCode(200);
+        getSpecRequest().post("/house/{houseId}/settle/{userId}", houseId, userId).then().log().all().statusCode(200);
     }
 
     @Step("Покупаем автомобиль с id = {carId}, пользователю с id = {userId}")
     public void buyCarByUserID(int carId, int userId) {
-        given().baseUri(appConfig.URL).contentType("application/json").filter(new AllureRestAssured())
-                .post("/user/{userId}/buyCar/{carId}", userId, carId).then().log().all().statusCode(200);
+        getSpecRequest().post("/user/{userId}/buyCar/{carId}", userId, carId).then().log().all().statusCode(200);
     }
 }
